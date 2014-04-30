f4f44e273b94f72b321c710a142cff231791ea0a
James Kerr <lords-death@hotmail.com>
2014-04-30 22:33:05 +1200

    Mess Up Fix
    Somehow I appended the previous version to the top, hopefuly fixed

--- Minecraft/src/main/java/Disconsented/ANSSRPG/CommonProxy.java
+++ Minecraft/src/main/java/Disconsented/ANSSRPG/CommonProxy.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Disconsented.ANSSRPG;
 
 import java.util.HashMap;
@@ -14,21 +13,4 @@ public class CommonProxy
 	public void registerRenderers() {
 	}
 
-=======
-package Disconsented.ANSSRPG;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import cpw.mods.fml.common.network.IGuiHandler;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.world.World;
-
-public class CommonProxy
-{
-	public void registerRenderers() {
-	}
-
->>>>>>> 50aaab4859915e5e919b0c73b5a29b869cba2644
 }
--- Minecraft/src/main/java/Disconsented/ANSSRPG/TestCommand.java
+++ Minecraft/src/main/java/Disconsented/ANSSRPG/TestCommand.java
@@ -1,62 +1,3 @@
-<<<<<<< HEAD
-package Disconsented.ANSSRPG;
-
-import java.util.List;
-
-import net.minecraft.command.ICommand;
-import net.minecraft.command.ICommandSender;
-
-public class TestCommand implements ICommand {
-
-	@Override
-	public int compareTo(Object arg0) {
-		// TODO Auto-generated method stub
-		return 0;
-		}
-
-	@Override
-	public String getCommandName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getCommandUsage(ICommandSender var1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List getCommandAliases() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void processCommand(ICommandSender var1, String[] var2) {
-		System.out.println("Command works :D");
-	}
-
-	@Override
-	public boolean canCommandSenderUseCommand(ICommandSender var1) {
-		// TODO Auto-generated method stub
-		return true;
-	}
-
-	@Override
-	public List addTabCompletionOptions(ICommandSender var1, String[] var2) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean isUsernameIndex(String[] var1, int var2) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-}
-=======
 package Disconsented.ANSSRPG;
 
 import java.util.List;
@@ -114,4 +55,4 @@ public class TestCommand implements ICommand {
 	}
 
 }
->>>>>>> 50aaab4859915e5e919b0c73b5a29b869cba2644
+
