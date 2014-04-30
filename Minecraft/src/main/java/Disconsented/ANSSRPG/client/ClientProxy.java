70d935d0887d05f872c0ea6a8db89cd9079463da
James Kerr <lords-death@hotmail.com>
2014-04-30 21:50:18 +1200

    Updating more files

---
+++ Minecraft/src/main/java/Disconsented/ANSSRPG/client/ClientProxy.java
@@ -0,0 +1,13 @@
+package Disconsented.ANSSRPG.client;
+
+import net.minecraftforge.client.MinecraftForgeClient;
+import Disconsented.ANSSRPG.CommonProxy;
+
+public class ClientProxy extends Disconsented.ANSSRPG.CommonProxy {
+       
+        @Override
+        public void registerRenderers() {
+                // This is for rendering entities and so forth later on
+        }
+       
+}
