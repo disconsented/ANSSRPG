4062bcdf526e2be4e1a801689021a42b78671936
James Kerr <lords-death@hotmail.com>
2014-04-30 21:34:37 +1200

    Revert "First Upload"
    This reverts commit 16e373f5b4efd9098c643a83caf4daf736300db5.

---
+++ Minecraft/src/main/java/Disconsented/ANSSRPG/CommonProxy.java
@@ -0,0 +1,16 @@
+package Disconsented.ANSSRPG;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import cpw.mods.fml.common.network.IGuiHandler;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+
+public class CommonProxy
+{
+	public void registerRenderers() {
+	}
+
+}
---
+++ Minecraft/src/main/java/Disconsented/ANSSRPG/TestCommand.java
@@ -0,0 +1,57 @@
+package Disconsented.ANSSRPG;
+
+import java.util.List;
+
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+
+public class TestCommand implements ICommand {
+
+	@Override
+	public int compareTo(Object arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getCommandName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender var1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List getCommandAliases() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void processCommand(ICommandSender var1, String[] var2) {
+		System.out.println("Command works :D");
+	}
+
+	@Override
+	public boolean canCommandSenderUseCommand(ICommandSender var1) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public List addTabCompletionOptions(ICommandSender var1, String[] var2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isUsernameIndex(String[] var1, int var2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
