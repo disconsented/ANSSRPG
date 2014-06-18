5ce3d50d25742e39ac671dbc4797d815b75886f4
James <lords-death@hotmail.com>
2014-06-18 17:52:45 +1200

    General Clean up

--- src/main/java/disconsented/anssrpg/data/SkillInfo.java
+++ src/main/java/disconsented/anssrpg/data/SkillInfo.java
@@ -15,7 +15,6 @@ import net.minecraft.item.Item;
 
 public class SkillInfo {
 	
-	public int skill;
 	public String name;
 	public byte type; //Handles the skill type block breaking[1](onBreakEvent), Entity Damaging[2](), Crafting[3](ItemCraftedEvent),  
 	public ArrayList exp, req = new ArrayList();
@@ -34,15 +33,6 @@ public class SkillInfo {
 		this.name = name;
 		this.itemName = itemName;
 		this.type = type;
-	}
-
-	public SkillInfo(int skill, ArrayList exp, ArrayList req, String name, byte type, ArrayList itemName) {
-		this.skill = skill;
-		this.exp = exp;
-		this.req = req;
-		this.name = name;
-		this.type = type;
-		this.itemName = itemName;
 	}
 
 }
