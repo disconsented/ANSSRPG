233b6f9a6e5aec0f602cd990166386ec3e96e8a9
James <lords-death@hotmail.com>
2014-06-18 22:37:12 +1200

    General Cleanup

--- src/main/java/disconsented/anssrpg/config/JsonConfigHandler.java
+++ src/main/java/disconsented/anssrpg/config/JsonConfigHandler.java
@@ -9,6 +9,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -17,11 +20,14 @@ import org.json.simple.parser.ParseException;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import javax.swing.JOptionPane;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import disconsented.anssrpg.data.PerkObject;
-import disconsented.anssrpg.data.SkillInfo;
+import disconsented.anssrpg.data.SkillHandler;
+import disconsented.anssrpg.data.SkillObject;
 /**
  * @author james
  * Json Config's
@@ -46,13 +52,17 @@ public class JsonConfigHandler {
 	static boolean debug = false;
 	static double levelCurve = 1.6;
 	
-	static ArrayList<SkillInfo> skillList = new ArrayList<SkillInfo>();
+	
 	static ArrayList skillNames = new ArrayList();
 	static ArrayList skillType = new ArrayList();
 	
-	
-	
-	public static void loadConfig() {	
+	public static void loadConfigs(){
+		loadConfig();
+		for(int i = 0; i < skillNames.size();i++){
+			loadSkillConfig(skillNames.get(i).toString());
+		}
+	}
+	private static void loadConfig() {	
 		JSONParser parser = new JSONParser();		 
 		try {	 
 			Object obj = parser.parse(new FileReader(configFile));	 
@@ -74,13 +84,8 @@ public class JsonConfigHandler {
 			System.out.println("ANSSRPG | "+PRINTOUT_ENTITY+ " = " + printOutEntity);
 	 
 			// loop array
-			JSONArray msg = (JSONArray) jsonObject.get(SKILL_LIST);
-			System.out.println(msg);
-			Iterator<String> iterator = msg.iterator();
-			while (iterator.hasNext()) {
-				System.out.println(iterator.next());
-				skillNames.add(iterator.next());
-			}
+			skillNames = (JSONArray) jsonObject.get(SKILL_LIST);
+			
 	 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -96,7 +101,7 @@ public class JsonConfigHandler {
 			createConfig();
 		}
 	}
-	public static void createConfig(){		
+	private static void createConfig(){		
 		JSONObject obj = new JSONObject();
 		obj.put(DEBUG_INFO, false);
 		obj.put(LEVEL_CURVE, 1.6);		
@@ -123,75 +128,97 @@ public class JsonConfigHandler {
 		} catch (IOException e) {
 		}
 	}
-	public static void loadSkillConfig(){
-		for(int i = 0;i < skillNames.size();i++){
-			JSONParser parser = new JSONParser();		 
-			try {	
-					ArrayList tempEntryName = new ArrayList();// Temps, remember to clear em after storing the data in the main array
-					ArrayList tempEXP = new ArrayList();
+	private static void loadSkillConfig(String name){		
+		JSONParser parser = new JSONParser();		 
+		try {	//If it isn't successful its just skipped
+				ArrayList tempEntryName = new ArrayList();// Temps, remember to clear em after storing the data in the main array
+				ArrayList tempEXP = new ArrayList();
+				
+				int tempType = 1;
 					
-					File skillConfig = new File("config/ANSSRPG",skillNames.get(i).toString().toLowerCase().replaceAll("[^A-Za-z0-9]", "")+".cfg");
-					Object obj = parser.parse(new FileReader(skillConfig));	 
-					JSONObject jsonObject = (JSONObject) obj;
-					JSONArray entryID = (JSONArray) jsonObject.get(BLOCK_NAME);
-						entryID = (JSONArray) jsonObject.get(ENTRY_NAME);					// loop array
+				File skillConfig = new File("config/ANSSRPG",name.replaceAll("[^A-Za-z0-9]", "")+".cfg");
+				Object obj = parser.parse(new FileReader(skillConfig));	 
+				JSONObject jsonObject = (JSONObject) obj;
+				JSONArray entryID = (JSONArray) jsonObject.get(BLOCK_NAME);
+				tempType = Integer.parseInt(jsonObject.get(SKILL_TYPE).toString());
+					
+				entryID = (JSONArray) jsonObject.get(ENTRY_NAME);					// loop array
 	
-						tempEntryName = entryID;
-						System.out.println("###############################################");
-						System.out.println(entryID);
+				tempEntryName = entryID; //testing directly placing the data into the arraylist form json
+				//JOptionPane.showMessageDialog(null, tempEntryName);
 					
-					JSONArray exp = (JSONArray) jsonObject.get(ENTRY_EXP);
-					System.out.println(exp);
-					Iterator<String> iterator1 = exp.iterator();
-					while (iterator1.hasNext()) {
-						System.out.println(iterator1.next());
-						tempEXP.add(iterator1.next());
-					}
-					System.out.println("TEMP ENTRY NAMES: "+tempEntryName);
-					System.out.println("TEMP EXP: "+tempEXP);
-		 
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-					createSkillConfig();
-				} catch (IOException e) {
-					e.printStackTrace();
-					createSkillConfig();
-				} catch (ParseException e) {
-					e.printStackTrace();
-					createSkillConfig();
-				} catch (NullPointerException e){
-					e.printStackTrace();
-					createSkillConfig();
-				}
+				tempEXP = (JSONArray) jsonObject.get(ENTRY_EXP);
+
+				tempEntryName = (JSONArray) jsonObject.get(ENTRY_EXP);
+				SkillObject tempObject = new SkillObject(tempEXP, name, tempEXP, 0);
+				SkillHandler.add(tempObject);
+
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+				createSkillConfig(name);
+			} catch (IOException e) {
+				e.printStackTrace();
+				createSkillConfig(name);
+			} catch (ParseException e) {
+				e.printStackTrace();
+				createSkillConfig(name);
+			} catch (NullPointerException e){
+				e.printStackTrace();
+				createSkillConfig(name);
 			}
 	}
-	public static void createSkillConfig(){		
-		for(int i = 0;i < skillNames.size();i++){
-			File skillConfig = new File("config/ANSSRPG",skillNames.get(i).toString().toLowerCase().replaceAll("[^A-Za-z0-9]", "")+".cfg");
-			JSONObject obj = new JSONObject();
-			JSONArray listEntryName = new JSONArray();
-			JSONArray listEntryXP = new JSONArray();
-			for ( int x = 0;i < 4; i++){
-				listEntryName.add("REPLACEMEH");
-			}
-			for ( int x = 0;i < 4; i++){
-				listEntryXP.add(1);
-			}		
+	private static void createSkillConfig(String name){		
+		File skillConfig = new File("config/ANSSRPG",name.replaceAll("[^A-Za-z0-9]", "")+".cfg");
+		JSONObject obj = new JSONObject();
+		obj.put(SKILL_TYPE, 1);
+		JSONArray listEntryName = new JSONArray();
+		JSONArray listEntryXP = new JSONArray();
+		for ( int i = 0;i < 4; i++){
+			listEntryName.add("REPLACEMEH");
+		}
+		for ( int i = 0;i < 4; i++){
+			listEntryXP.add(1);
+		}		
 	
-			obj.put(ENTRY_NAME,listEntryName);		
-			obj.put(ENTRY_EXP,listEntryXP);
-			try {		 
-					FileWriter file = new FileWriter(skillConfig);
-					file.write(obj.toJSONString());
-					file.flush();
-					file.close();	 
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+		obj.put(ENTRY_NAME,listEntryName);		
+		obj.put(ENTRY_EXP,listEntryXP);
+		try {		 
+				FileWriter file = new FileWriter(skillConfig);
+				file.write(obj.toJSONString());
+				file.flush();
+				file.close();	 
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
-	public void createPerkConfig(){
-		JSONObject obj = new JSONObject();
+	/**
+	 * Single file for all perks
+	 * each obj entry will be a perk
+	 */
+	private void createPerkConfig(){
+		JSONObject obj = new JSONObject();		
+		//String name, Block unlockBlock, String description, int pointCost, ArrayList requirementName, ArrayList requirementLevel
+		ArrayList tempName = new ArrayList();
+		ArrayList tempLevel = new ArrayList();
+		tempName.add("Foo");
+		tempLevel.add(1);
+		tempName.add("Bar");
+		tempLevel.add(5);
+		tempName.add("FooBar");
+		tempLevel.add(10);
+		PerkObject temp = new PerkObject("Mine Diamond", Blocks.diamond_ore, "Ermagawd diminds!", 5, tempName, tempLevel);
+		ArrayList tempName2 = new ArrayList();
+		ArrayList tempLevel2 = new ArrayList();
+		tempName2.add("ooF");
+		tempLevel.add(1);
+		tempName2.add("raB");
+		tempLevel.add(5);
+		tempName2.add("RabFoo");
+		tempLevel.add(10);
+		PerkObject temp2 = new PerkObject("Mine Diamond", Blocks.diamond_ore, "Ermagawd diminds!", 5, tempName, tempLevel);
+		obj.put("Example 1", temp);
+		obj.put("Example 2", temp2);
+		
 		try {
 			 
 			FileWriter file = new FileWriter(configFile);
@@ -203,7 +230,7 @@ public class JsonConfigHandler {
 			e.printStackTrace();
 		}
 	}
-	public void loadPerkConfig(){
+	private void loadPerkConfig(){
 		
 	}
 	
@@ -218,9 +245,6 @@ public class JsonConfigHandler {
 	}
 	public static boolean getPrintEntity(){
 		return printOutEntity;
-	}
-	public static ArrayList getSkillList(){
-		return skillList;
 	}
 	public static double getLevelCurve() {
 		return levelCurve;
