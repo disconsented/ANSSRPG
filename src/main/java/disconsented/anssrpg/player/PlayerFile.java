40320ad8604d13e1b0061d9021875d66f34e08ea
James <lords-death@hotmail.com>
2014-07-10 19:02:56 +1200

    PlayerFile update
    Now Stores and reads player data with Gson

--- src/main/java/disconsented/anssrpg/player/PlayerFile.java
+++ src/main/java/disconsented/anssrpg/player/PlayerFile.java
@@ -1,26 +1,47 @@
 package disconsented.anssrpg.player;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import scala.util.parsing.input.StreamReader;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import disconsented.anssrpg.data.DataSave;
 import net.minecraft.world.WorldSavedData;
 
 public class PlayerFile {
 	static String PERK_LIST = "Perk List";
 	static String SKILL_LIST = "Skill List";
+	static File dataFolder = new File("data");
 	
 	
 	public static void loadPlayer(String playerID) {
+		File dataLocation = new File(dataFolder, playerID);
+		try{
+			FileReader reader = new FileReader(dataLocation);
+            Gson gson = new GsonBuilder().create();
+            PlayerData p = gson.fromJson(reader, PlayerData.class);
+            System.out.println(p);
+            }
+		catch(FileNotFoundException e){
+			DataSave.createPlayer(playerID);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 	/**
 	 * Write all of the player data to a file
 	 * 
 	 */
 
-	public static void writePlayer(PlayerData player){	
-		File dataFolder = new File("data");
+	public static void writePlayer(PlayerData player){
 		File dataLocation = new File(dataFolder, player.playerID);
 		dataFolder.mkdirs();
 		Gson gson = new Gson();  
