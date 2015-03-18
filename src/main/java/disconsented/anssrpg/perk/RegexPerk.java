02470e230dfb03829a3b46a59336afa6d0e6523b
James <lords-death@hotmail.com>
2015-03-18 20:38:16 +1300

    Removed Regex support

--- src/main/java/disconsented/anssrpg/perk/RegexPerk.java
+++
@@ -1,9 +0,0 @@
-package disconsented.anssrpg.perk;
-
-import com.google.gson.annotations.Expose;
-
-public abstract class RegexPerk extends Perk {
-
-    @Expose
-    public String searchQuery = "default_searchQuery";
-}
