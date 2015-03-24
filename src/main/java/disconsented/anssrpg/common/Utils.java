fe22e25e00287c7524b1d6123be5504c88fc5c47
James <lords-death@hotmail.com>
2015-03-24 16:24:45 +1300

    Added Util's class

---
+++ src/main/java/disconsented/anssrpg/common/Utils.java
@@ -0,0 +1,50 @@
+/*The MIT License (MIT)
+
+Copyright (c) 2015 Disconsented, James Kerr
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ */
+package disconsented.anssrpg.common;
+
+public class Utils {
+    /**
+     * Returns 2 if two objects are matched. If either Metadata is <= -1 then it is treated as a wildcard (Metadata is ignored)
+     * @param object1
+     * @param metadata1
+     * @param object2
+     * @param metadata2
+     * @return
+     */
+    public static boolean MatchObject(Object object1, int metadata1, Object object2, int metadata2){
+        if (metadata1 > -1 && metadata2 > -1){
+            if (object1.equals(object2) && metadata1 == metadata2){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (object1.equals(object2)){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+}
