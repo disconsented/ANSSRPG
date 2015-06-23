6cb76f45dda88feb366da97c3a54e8c776c522d4
James <lords-death@hotmail.com>
2015-06-23 19:27:09 +1200

    Repurposed PerkGuiContainer to a dummy for GUI's

--- src/main/java/disconsented/anssrpg/gui/DummyContainer.java
+++ src/main/java/disconsented/anssrpg/gui/DummyContainer.java
@@ -25,7 +25,7 @@ package disconsented.anssrpg.gui;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
 
-public class PerkGUIContainer extends Container {
+public class DummyContainer extends Container {
 
     @Override
     public boolean canInteractWith(EntityPlayer p_75145_1_) {
